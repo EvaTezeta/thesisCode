7bf2a921a53d23f5b90a5280c78c557738933306
Eva <evatschotanus@gmail.com>
2023-03-02 16:57:23 +0100

    Data exploration tables

--- Merged_Data.R
+++ Merged_Data.R
@@ -1,4 +1,64 @@
+#Loading libraries
+library(dplyr) #For pipes
+library(knitr) #For tables
+library(tidyr) #For pivor_wider()
+
 #Merge three datasets together
 data_merged <- rbind(DataNL, DataSL, DataEN)
 View(data_merged)
+
+#Change data to correct data type
+data_merged$Sex <- as.factor(data_merged$Sex)
+data_merged$`Age Group` <- as.factor(data_merged$`Age Group`)
+data_merged$Country <- as.factor(data_merged$Country)
+
+##Data exploration tables
+#Year table
+year_table <- data_merged %>%
+  group_by(Country, Year) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = "Country", values_from = "n", values_fill = 0) %>%
+  as.data.frame() %>%
+  `row.names<-`(.[,1]) %>%
+  .[,-1] %>%
+  kable()
+
+print(year_table)
+
+#Age table by country
+age_table <- data_merged %>%
+  group_by(Country, `Age Group`) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = "Country", values_from = "n", values_fill = 0) %>%
+  as.data.frame() %>%
+  `row.names<-`(.[,1]) %>%
+  .[,-1] %>%
+  kable()
+
+print(age_table)
+
+#Sex table by country
+sex_table <- data_merged %>%
+  group_by(Country, Sex) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = "Country", values_from = "n", values_fill = 0) %>%
+  as.data.frame() %>%
+  `row.names<-`(.[,1]) %>%
+  .[,-1] %>%
+  kable
+
+print(sex_table)
+
+#Death category table by country
+death_table <- data_merged %>%
+  group_by(Country, `Death category`) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = "Country", values_from = "n", values_fill = 0) %>%
+  as.data.frame() %>%
+  `row.names<-`(.[,1]) %>%
+  .[,-1] %>%
+  kable()
+
+print(death_table)
+
 
