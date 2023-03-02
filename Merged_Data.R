9c3ab9ae8f9e309a87ed74aaf50c8fc58e76857d
Eva <evatschotanus@gmail.com>
2023-03-02 18:14:45 +0100

    Graphs

--- Merged_Data.R
+++ Merged_Data.R
@@ -1,7 +1,8 @@
 #Loading libraries
 library(dplyr) #For pipes
 library(knitr) #For tables
-library(tidyr) #For pivor_wider()
+library(tidyr) #For pivot_wider()
+library(ggplot2) #For graphs
 
 #Merge three datasets together
 data_merged <- rbind(DataNL, DataSL, DataEN)
@@ -11,6 +12,10 @@ View(data_merged)
 data_merged$Sex <- as.factor(data_merged$Sex)
 data_merged$`Age Group` <- as.factor(data_merged$`Age Group`)
 data_merged$Country <- as.factor(data_merged$Country)
+
+#Convert month to an ordered factor variable
+data_merged$Month <- factor(data_merged$Month, levels = 1:12, ordered = TRUE)
+
 
 ##Data exploration tables
 #Year table
@@ -60,5 +65,72 @@ death_table <- data_merged %>%
   kable()
 
 print(death_table)
+
+##Plots for data exploration
+
+#Histogram of year divided by country
+ggplot(data_merged, aes(x = Year, fill = Country)) +
+  geom_histogram(binwidth = 1, alpha = 0.5, position = "dodge") +
+  facet_wrap(~ Country, ncol = 3) +
+  labs(x = "Year", y = "Frequency") +
+  theme(legend.position = "bottom")
+
+#Histogram of blubber thickness averages by country
+ggplot(data_merged, aes(x = `BT Average`, fill = Country)) +
+  geom_histogram(binwidth = 1, alpha = 0.5, position = "dodge") +
+  facet_wrap(~ Country, ncol = 3) +
+  labs(x = "Blubber thickness averages (mm)", y = "Frequency") +
+  theme(legend.position = "bottom")
+
+#Bar plot of sex by country
+ggplot(data_merged, aes(x = Country, fill = Sex)) +
+  geom_bar(position = "dodge") +
+  labs(x = "Country", y = "Count", fill = "Sex") +
+  ggtitle("Sex distribution by country") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#Bar plot of age by country
+ggplot(data_merged, aes(x = Country, fill = `Age Group`)) +
+  geom_bar(position = "dodge") +
+  labs(x = "Country", y = "Count", fill = "Age Group") +
+  ggtitle("Age group distribution by country") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#Bar plot of death category by country
+ggplot(data_merged, aes(x = Country, fill = `Death category`)) +
+  geom_bar(position = "dodge") +
+  labs(x = "Country", y = "Count", fill = "Death category") +
+  ggtitle("Death category distribution by country") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+# Calculate the mean blubber thickness by country and month
+avg_blubber <- aggregate(`BT Average` ~ Country + Month, data = data_merged, FUN = mean)
+
+# create a new data frame with the average blubber thickness per month and country
+avg_blubber <- data_merged %>%
+  group_by(Month, Country) %>%
+  summarise(avg_blubber_thickness = mean(`BT Average`))
+
+# plot the average blubber thickness per month and country
+ggplot(avg_blubber, aes(x = Month, y = avg_blubber_thickness, color = Country)) +
+  geom_line() +
+  labs(title = "Average Blubber Thickness", 
+       x = "Month",
+       y = "Average Blubber Thickness (mm)") +
+  scale_color_manual(values = c("red", "blue", "green")) +
+  theme_bw()
+
+
+
+# Calculate the average blubber thickness for each month and country
+avg_bt <- aggregate(`BT Average` ~ Month + Country, data = data_merged, FUN = mean)
+
+# Plot the average blubber thickness over time for each country as a separate line
+ggplot(data = avg_bt, aes(x = Month, y = `BT Average`, group = Country, color = Country)) +
+  geom_line() +
+  labs(x = "Month", y = "Average blubber thickness (mm)") +
+  scale_color_discrete(name = "Country") +
+  scale_y_continuous(limits = c(10, 22.5), breaks = seq(10, 22.5, by = 2))
 
 
