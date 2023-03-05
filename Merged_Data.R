e841d73a83d977075fa16c45d40bfeaaff61cf01
Eva <evatschotanus@gmail.com>
2023-03-05 15:57:10 +0100

    Temp data added

--- Merged_Data.R
+++ Merged_Data.R
@@ -16,6 +16,43 @@ data_merged$Country <- as.factor(data_merged$Country)
 #Convert month to an ordered factor variable
 data_merged$Month <- factor(data_merged$Month, levels = 1:12, ordered = TRUE)
 
+# Create a new column called "SST"
+data_merged$SST <- NA
+
+# Loop through each row of data_merged
+for (i in 1:nrow(data_merged)) {
+  lat <- data_merged$`WGS84-Lat`[i]
+  year <- data_merged$Year[i]
+  month <- data_merged$Month[i]
+  
+# Check for missing values in latitude column
+if (is.na(lat)) {
+  # If missing value, assign NA to SST column
+  data_merged$SST[i] <- NA
+} else {
+  # Determine which dataframe to use based on latitude
+  if (!is.na(lat) && lat >= 55) {
+    sst_df <- north_sst
+  } else {
+    sst_df <- south_sst
+  }
+  
+  # Find corresponding temperature value in sst_df
+  temp <- sst_df$temp[sst_df$year == year & sst_df$month == month]
+  
+  # Check if a temperature value exists
+  if (length(temp) == 0) {
+    # If no value exists, assign NA to SST column
+    data_merged$SST[i] <- NA
+  } else {
+    # Assign temperature value to SST column in data_merged
+    data_merged$SST[i] <- temp
+  }
+}
+}
+
+
+View(data_merged)
 
 ##Data exploration tables
 #Year table
@@ -103,7 +140,6 @@ ggplot(data_merged, aes(x = Country, fill = `Death category`)) +
   ggtitle("Death category distribution by country") +
   theme(plot.title = element_text(hjust = 0.5))
 
-
 # Calculate the mean blubber thickness by country and month
 avg_blubber <- aggregate(`BT Average` ~ Country + Month, data = data_merged, FUN = mean)
 
@@ -120,8 +156,6 @@ ggplot(avg_blubber, aes(x = Month, y = avg_blubber_thickness, color = Country)) 
        y = "Average Blubber Thickness (mm)") +
   scale_color_manual(values = c("red", "blue", "green")) +
   theme_bw()
-
-
 
 # Calculate the average blubber thickness for each month and country
 avg_bt <- aggregate(`BT Average` ~ Month + Country, data = data_merged, FUN = mean)
