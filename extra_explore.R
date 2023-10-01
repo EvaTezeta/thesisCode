fee959914ed0d45b76e45d13202c5eb192fb311c
Eva <evatschotanus@gmail.com>
2023-10-01 12:41:10 +0200

    Final changes 1/10

--- extra_explore.R
+++ extra_explore.R
@@ -41,6 +41,33 @@ kable(formatted_avg_bt_wide,
       caption = "Monthly Average BT for Females and Males in South and North Regions") %>%
   kable_styling(bootstrap_options = "striped")
 
+###########
+# Filter the data to include only Age_class A and J
+filtered_data <- data_merged %>%
+  filter(Age_class %in% c("A", "J"))
+
+# Calculate the average BT for each Month, Age_class and Region
+avg_bt_per_month_age_region <- filtered_data %>%
+  group_by(Month, Age_class, region) %>%
+  summarise(Avg_BT = mean(`BT Average`))
+
+# Reshape the data to wide format
+avg_bt_wide <- avg_bt_per_month_age_region %>%
+  pivot_wider(names_from = c(Age_class, region), values_from = Avg_BT)
+
+# Format the values to show only one decimal place
+formatted_avg_bt_wide <- avg_bt_wide %>%
+  mutate(across(everything(), ~ sprintf("%.1f", .)))
+
+# Display the table using kable
+kable(formatted_avg_bt_wide,
+      row.names = FALSE,
+      format = "markdown",
+      caption = "Monthly Average BT for Age Classes A and J in South and North Regions") %>%
+  kable_styling(bootstrap_options = "striped")
+
+
+
 
 ############# 
 
@@ -100,6 +127,124 @@ plot_grid1 <- plot_grid(plotlist = plot_list1, ncol = 2)
 
 #Print plot
 plot_grid1
+
+#-----------------
+
+##Plot for monthly BT per sex and region
+# Set the y-axis limits
+y_limits <- c(14, 21)
+
+# Create list to store the plots
+plot_list2 <- list()
+
+# Create three separate plots, one for each country
+for (i in unique(data_merged$region)) {
+  
+  # Subset the data for the current country
+  region_data <- subset(data_merged, region == i)
+  
+  # Calculate the average BT for each month, sex, and region
+  avg_bmi <- aggregate(BMI ~ Month + Sex, data = region_data, FUN = mean)
+  
+  # Plot the average BT over time for each sex as a separate line
+  p2 <- ggplot(data = avg_bmi, aes(x = Month, y = BMI, group = Sex, color = Sex)) +
+    geom_line() +
+    scale_color_manual(values = my_colors, labels = c("Female", "Male")) +
+    labs(x = "Month", y = "Average BMI", title = i) +
+    theme_bw() +
+    scale_y_continuous(limits = y_limits)
+  
+  #Add plots to list
+  plot_list2[[i]] <- (p2)
+  
+}
+
+#Arrange plots into grid
+plot_grid2 <- plot_grid(plotlist = plot_list2, ncol = 2)
+
+#Print plot
+plot_grid2
+
+#-------
+
+# Set the y-axis limits
+y_limits <- c(15, 22)
+
+# Create list to store the plots
+plot_list3 <- list()
+
+# Create three separate plots, one for each region
+for (i in unique(data_merged$region)) {
+  
+  # Subset the data for the current region
+  region_data <- subset(data_merged, region == i)
+  
+  # Filter for Age_class values "J" and "A"
+  region_data_filtered <- subset(region_data, Age_class %in% c("J", "A"))
+  
+  # Calculate the average BMI for each month, Age_class, and region
+  avg_bmi <- aggregate(BMI ~ Month + Age_class, data = region_data_filtered, FUN = mean)
+  
+  # Plot the average BMI over time for each Age_class as a separate line
+  p3 <- ggplot(data = avg_bmi, aes(x = Month, y = BMI, group = Age_class, color = Age_class)) +
+    geom_line() +
+    scale_color_manual(values = my_colors, labels = c("Adult", "Juvenile")) +
+    labs(x = "Month", y = "Average BMI", title = paste(i)) +
+    theme_bw() +
+    scale_y_continuous(limits = y_limits)
+  
+  # Add plots to list
+  plot_list3[[i]] <- (p3)
+  
+}
+
+# Arrange plots into a grid
+plot_grid3 <- plot_grid(plotlist = plot_list3, ncol = 2)
+
+# Print the plot
+plot_grid3
+
+#--------
+# Set the y-axis limits
+y_limits <- c(11, 20.5)
+
+# Create list to store the plots
+plot_list4 <- list()
+
+# Create three separate plots, one for each region
+for (i in unique(data_merged$region)) {
+  
+  # Subset the data for the current region
+  region_data <- subset(data_merged, region == i)
+  
+  # Filter for Age_class values "J" and "A"
+  region_data_filtered <- subset(region_data, Age_class %in% c("J", "A"))
+  
+  # Calculate the average 'BT Average' for each month, Age_class, and region
+  avg_bt <- aggregate(`BT Average` ~ Month + Age_class, data = region_data_filtered, FUN = mean)
+  
+  # Plot the average 'BT Average' over time for each Age_class as a separate line
+  p4 <- ggplot(data = avg_bt, aes(x = Month, y = `BT Average`, group = Age_class, color = Age_class)) +
+    geom_line() +
+    scale_color_manual(values = my_colors, labels = c("Adult", "Juvenile")) +
+    labs(x = "Month", y = "Average BT", title = paste(i)) +
+    theme_bw() +
+    scale_y_continuous(limits = y_limits)
+  
+  # Add plots to list
+  plot_list4[[i]] <- (p4)
+  
+}
+
+# Arrange plots into a grid
+plot_grid4 <- plot_grid(plotlist = plot_list4, ncol = 2)
+
+# Print the plot
+plot_grid4
+
+
+
+
 
 ######### SST plot
 
@@ -176,10 +321,31 @@ scatter_plot <- ggplot(DataNL_subset, aes(x = DayOfYear, y = `BT Average`, color
 print(scatter_plot)
 
 
+##############
 
+# Calculate the average BT Average for each month, Sex, and Country
+avg_bt_sex_country <- aggregate(`BT Average` ~ Month + Sex + Country, data = data_merged, FUN = mean)
 
+# Create a plot for both Sex "F" and "M" with facets by Country
+ggplot(data = subset(avg_bt_sex_country, Sex %in% c("F", "M")), 
+       aes(x = Month, y = `BT Average`, group = Sex, color = Sex)) +
+  geom_line() +
+  scale_color_manual(values = my_colors, name = "Sex", labels = c("Female", "Male")) +
+  labs(x = "Month", y = "Average BT", title = "Average BT per Month for Female and Male Porpoises") +
+  facet_wrap(~Country) +  # Add facets for each Country
+  theme_bw()
 
+# Calculate the average BT Average for each month, Age_class, and Country
+avg_bt_age_country <- aggregate(`BT Average` ~ Month + `Age_class` + Country, data = data_merged, FUN = mean)
 
+# Create a plot for both Age_classs "J" and "A" with facets by Country
+ggplot(data = subset(avg_bt_age_country, `Age_class` %in% c("J", "A")), 
+       aes(x = Month, y = `BT Average`, group = `Age_class`, color = `Age_class`)) +
+  geom_line() +
+  scale_color_manual(values = my_colors, name = "Age Class", labels = c("Adult", "Juvenile")) +
+  labs(x = "Month", y = "Average BT", title = "Average BT per Month for Juvenile and Adult Porpoises") +
+  facet_wrap(~Country) +  # Add facets for each Country
+  theme_bw()
 
 
 
